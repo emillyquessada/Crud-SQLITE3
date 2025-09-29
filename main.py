06775d07a2226004b2fbc03ccd3078bdb0f2bbc7
Emilly <emillyquessada26@gmail.com>
2025-09-29 14:55:34 -0300

    2. Função de cadastro de livros

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ CREATE TABLE IF NOT EXISTS livros (
 """)
 print("Tabela criada com sucesso!")
 
+#Etapa 2
+def cadastrar_livro(): 
+    titulo = input("Digite o nome do livro que deseja cadastrar: ")
+    autor = input("Digite o nome do autor: ")
+    ano = int(input("Digite o ano de lançamento: "))
+
+    cursor.execute("""
+    INSERT INTO livros (título, autor, ano, disponível)
+    VALUES (?, ?, ?, ?)
+    """, (titulo, autor, ano, "sim" )
+    )
+    conexao.commit()
+
+cadastrar_livro()
